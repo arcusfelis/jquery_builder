2e53d44112fbfaf1deb0e0b7b277acd27096709f
Uvarov Michael <freeakk@gmail.com>
2012-01-04 22:27:16 +0400

    jQuery downloader on github  hosting.:

---
+++ cron.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+URL="http://code.jquery.com/jquery.min.js"
+DIR="/home/builder/jquery-latest"
+COMMENT="Push latest version at $(date -u)."
+
+set -e 
+
+cd "$DIR"
+
+DEST_FILE="$(mktemp)"
+wget "${URL}" -O "${DEST_FILE}"
+
+mv "${DEST_FILE}" ./
+
+git add .
+git commit -m "${COMMENT}"
+git push
+
+set -e
